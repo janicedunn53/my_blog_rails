8f25225c461e215795a888bb3319637399b67f6b
Janice V. Dunn <janicedunn53@gmail.com>
2015-10-16 11:35:40 -0700

    Add and save a new post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,25 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
   def show
     @post = Post.find(params[:id])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
